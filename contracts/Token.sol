6ebda82622795692063c3355a1c24e45b46bbfba
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-03-30 13:31:27 +0300

    init

---
+++ contracts/Token.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.2;
+
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+import 'zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
+
+contract Token is MintableToken, BurnableToken {
+    string public constant name = "EURM";
+    string public constant symbol = "EURM";
+    uint8 public constant decimals = 2;
+
+    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    function Token() public {
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+    }
+}
