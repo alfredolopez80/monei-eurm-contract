f29fd4fc2d2ae2776f74f13508529c30ca4dc940
Dmitriy Nevzorov <jimmy.lugat@gmail.com>
2021-02-07 19:59:41 +0100

    update dependencies

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,21 +1,27 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.5.0;
 
-import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
-import 'zeppelin-solidity/contracts/token/ERC20/BurnableToken.sol';
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract Token is MintableToken, BurnableToken {
-    string public constant name = "EURM";
-    string public constant symbol = "EURM";
-    uint8 public constant decimals = 2;
-
-    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+/**
+ * @title TanganyTestToken
+ * @dev Very simple ERC20 Token example, where all 10000 tokens are pre-assigned to the creator.
+ */
+contract Token is ERC20, ERC20Detailed, ERC20Burnable {
+    // modify token name
+    string public constant NAME = "EURM";
+    // modify token symbol
+    string public constant SYMBOL = "EURM";
+    // modify token decimals
+    uint8 public constant DECIMALS = 2;
+    // modify initial token supply
+    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(DECIMALS)); // 10000 tokens
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    function Token() public {
-        totalSupply_ = INITIAL_SUPPLY;
-        balances[msg.sender] = INITIAL_SUPPLY;
-        Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+    constructor () public ERC20Detailed(NAME, SYMBOL, DECIMALS) {
+        _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
